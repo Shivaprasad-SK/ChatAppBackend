85be12f08853a78d1f57ca3a85dc2ca4aa515ed6
Shivaprasad K <144586350+Shivaprasad-SK@users.noreply.github.com>
2025-07-04 00:31:19 +0530

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIo(server, {
   cors: {
-    origin: process.env.FRONTEND_URL,
+    origin: "https://chats-mocha.vercel.app",
     methods: ["GET", "POST"],
     credentials: true,
   },
@@ -21,7 +21,7 @@ const io = socketIo(server, {
 // Middleware
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL,
+    origin: "https://chats-mocha.vercel.app",
     methods: ["GET", "POST"],
     credentials: true,
   })
